029e65c914e8259a4be0e7acd70cadf91c24a74e
seitvalieva <seitvalieva@gmail.com>
2024-06-13 11:31:59 +0200

    login case

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Se connecter</title>
+</head>
+<body>
+    <h1>Se connecter</h1>
+    <form action=" " method="POST">
+
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email"><br><br>
+
+        <label for="password">Mot de pass</label>
+        <input type="password" name="password" id="password"><br><br>
+
+
+        <input type="submit" value="Se connecter">
+    </form>
+    
+</body>
+</html>
--- traitement.php
+++ traitement.php
@@ -4,50 +4,94 @@
         switch($_GET["action"]) {
 
             case "register":
-                // connection to the database
-                $pdo = new PDO("mysql:host=localhost;dbname=php_hash_sev;charset=utf8", "root", "");
 
-                $pseudo =  filter_input(INPUT_POST, "pseudo", FILTER_SANITIZE_FULL_SPECIAL_CHARS);     // name attribute of the same input field
-                $email =  filter_input(INPUT_POST, "email", FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_EMAIL); // FILTERING the inputs of corresponding fields of reg form
-                $password =  filter_input(INPUT_POST, "password1", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-                $password2 =  filter_input(INPUT_POST, "password2", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                // if the form is submitted 
+                if($_POST["submit"]) {
 
-                if($pseudo && $email && $password && $password2){
-                    // var_dump("ok"); die;
-
-                    $requete = $pdo->prepare("
-                        SELECT *
-                        FROM user
-                        WHERE email = :email
-                    ");
-                    $requete->execute(["email" => $email]);
-
-                    $user = $requete->fetch();  // store the result of the request in $user
-                    
-                    //if user exists
-                    if($user) {
-                        header("Location: register.php"); exit;         // redirect the user to registration page
-                    } else {
-                        // var_dump("User doesnt exist"); die;
-                        // insert the user in the database
-
-                        if($password == $password2 && strlen($password) >= 8) {     // VERIFY both passwords are identic, and length of the pass min 8
-                            
-                            $insertUser = $pdo->prepare("
-                                INSERT INTO user (pseudo, email, password) 
-                                VALUES (:pseudo, :email, :password)
-                            ");
-                            
-                            $insertUser->execute([
-                                "pseudo" => $pseudo,
-                                "email" => $email,
-                                "password" => password_hash($password, PASSWORD_DEFAULT)
-                            ]);
-                            header("Location: login.php"); exit;        // redirect the user to login page
+                    // connection to the database
+                    $pdo = new PDO("mysql:host=localhost;dbname=php_hash_sev;charset=utf8", "root", "");
+    
+                    $pseudo =  filter_input(INPUT_POST, "pseudo", FILTER_SANITIZE_FULL_SPECIAL_CHARS);     // name attribute of the same input field
+                    $email =  filter_input(INPUT_POST, "email", FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_EMAIL); // FILTERING the inputs of corresponding fields of reg form
+                    $password =  filter_input(INPUT_POST, "password1", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                    $password2 =  filter_input(INPUT_POST, "password2", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    
+                    if($pseudo && $email && $password && $password2){
+                        // var_dump("ok"); die;
+    
+                        $requete = $pdo->prepare("
+                            SELECT *
+                            FROM user
+                            WHERE email = :email
+                        ");
+                        $requete->execute(["email" => $email]);
+    
+                        $user = $requete->fetch();  // store the result of the request in $user
+                        
+                        //if user exists
+                        if($user) {
+                            header("Location: register.php"); exit;         // redirect the user to registration page
+                        } else {
+                            // var_dump("User doesnt exist"); die;
+                            // insert the user in the database
+    
+                            if($password == $password2 && strlen($password) >= 8) {     // VERIFY both passwords are identic, and length of the pass min 8
+                                
+                                $insertUser = $pdo->prepare("
+                                    INSERT INTO user (pseudo, email, password) 
+                                    VALUES (:pseudo, :email, :password)
+                                ");
+                                
+                                $insertUser->execute([
+                                    "pseudo" => $pseudo,
+                                    "email" => $email,
+                                    "password" => password_hash($password, PASSWORD_DEFAULT)
+                                ]);
+                                header("Location: login.php"); exit;        // redirect the user to login page
+                            } else {
+    
+                                // message "The password is short"
+    
+                            }
                         }
+                    } else {
+                        // probleme de saisie dans les champs de formulaire
                     }
                 }
+                header("Location: register.php"); exit;
+            break;
 
+
+           case "login":
+
+                if($_POST["submit"]){
+                    // connection to the app
+                    $pdo = new PDO("mysql:host=localhost;dbname=php_hash_sev;charset=utf8", "root", "");
+                    
+                    $email =  filter_input(INPUT_POST, "email", FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_EMAIL); // FILTERING the inputs of corresponding fields of reg form
+                    $password =  filter_input(INPUT_POST, "password1", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+                    if($email && $password){
+                        $requete = $pdo->prepare("
+                            SELECT *
+                            FROM user
+                            WHERE email = :email
+                        ");
+                        $requete->execute(["email" => $email]);
+                        $user = $requete->fetch();
+                        var_dump($user); die;
+                    }
+
+                }
+
+                header("Location: login.php"); exit;
+
+
+            break;
+
+
+            case "logout":
+                // connection to the app
 
             break;
         }
